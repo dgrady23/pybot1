195e2340c1c47e3226a26ab375857f2b895cd8f0
dgrady23 <95245849+dgrady23@users.noreply.github.com>
2022-10-31 12:38:20 -0400

    quotes?

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import discord
 import os
+import requests
 import json
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,6 +10,13 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
 
 @client.event
 async def on_ready():
